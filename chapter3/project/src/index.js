aedd3b05d535800d394fcdc2e783eee03114c378
Yuzunoha <studysteadystandby@gmail.com>
2022-06-09 15:13:50 +0900

    押された

--- chapter3/project/src/index.js
+++ chapter3/project/src/index.js
@@ -1,15 +1,23 @@
-const p = console.log;
+const main = () => {
+  const p = console.log;
 
-const addContainerForBody = (text) => {
-  const bodyEl = document.getElementsByTagName('body')[0];
-  const newContainerEl = document.createElement('div');
-  newContainerEl.className = 'container';
-  const pEl = document.createElement('p');
-  pEl.textContent = text;
-  newContainerEl.appendChild(pEl);
-  bodyEl.appendChild(newContainerEl);
+  const addContainerForBody = (text) => {
+    const bodyEl = document.getElementsByTagName('body')[0];
+    const newContainerEl = document.createElement('div');
+    newContainerEl.className = 'container';
+    const pEl = document.createElement('p');
+    pEl.textContent = text;
+    newContainerEl.appendChild(pEl);
+    bodyEl.appendChild(newContainerEl);
+  };
+
+  const onClickAddMemo = () => {
+    alert('押された');
+  };
+
+  const addButtonEl = document.getElementById('add-button');
+
+  addButtonEl.addEventListener('click', onClickAddMemo);
 };
 
-addContainerForBody(1);
-addContainerForBody(2);
-addContainerForBody(3);
+document.addEventListener('DOMContentLoaded', main);
