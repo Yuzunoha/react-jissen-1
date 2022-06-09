190e4958eb5a855b0214eb14317aad55853ac7a7
Yuzunoha <studysteadystandby@gmail.com>
2022-06-09 18:24:09 +0900

    ボタンはこうやるらしい

--- chapter4/my-app/src/App.jsx
+++ chapter4/my-app/src/App.jsx
@@ -1,9 +1,13 @@
 export const App = () => {
+  // ボタン押下時に呼ぶ関数
+  const onClickButton = () => {
+    alert();
+  };
   return (
     <>
       <h1>こんにちは！！</h1>
       <p>お元気ですか。</p>
-      <button>ボタン</button>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
