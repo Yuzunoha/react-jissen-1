b2567a673117b30414341a8f3bb36dadbbfe03b1
Yuzunoha <studysteadystandby@gmail.com>
2022-06-09 15:07:10 +0900

    自作関数

--- chapter3/project/src/index.js
+++ chapter3/project/src/index.js
@@ -1,5 +1,15 @@
 const p = console.log;
 
-const containers = document.getElementsByClassName('container');
+const addContainerForBody = (text) => {
+  const bodyEl = document.getElementsByTagName('body')[0];
+  const newContainerEl = document.createElement('div');
+  newContainerEl.className = 'container';
+  const pEl = document.createElement('p');
+  pEl.textContent = text;
+  newContainerEl.appendChild(pEl);
+  bodyEl.appendChild(newContainerEl);
+};
 
-p(containers);
+addContainerForBody(1);
+addContainerForBody(2);
+addContainerForBody(3);
