b5ffdc64b9466dd12faecf03f12dd33a50b193c5
Yuzunoha <studysteadystandby@gmail.com>
2022-06-15 01:48:47 +0900

    props

--- chapter4/my-app/src/App.jsx
+++ chapter4/my-app/src/App.jsx
@@ -16,7 +16,7 @@ export const App = () => {
   return (
     <>
       <h1 style={styleH1}>こんにちは！！</h1>
-      <ColoredMessage />
+      <ColoredMessage color='blue' message='お元気ですか？' />
       <p style={contentPinkStyle}>元気です！</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
