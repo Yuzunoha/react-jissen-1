02e0b0b642bdd8fa63040f6f5a344dec4cef3f23
Yuzunoha <studysteadystandby@gmail.com>
2022-06-15 01:25:52 +0900

    リアルタイムだ

--- chapter4/my-app/src/App.jsx
+++ chapter4/my-app/src/App.jsx
@@ -3,11 +3,24 @@ export const App = () => {
   const onClickButton = () => {
     alert();
   };
+
+  // CSSオブジェクト
+  const contentStyle = {
+    color: 'blue',
+    fontSize: '20px',
+  };
+  // ピンクように追加
+  const contentPinkStyle = {
+    color: 'pink',
+    fontSize: '20px',
+  };
+
   const styleH1 = { color: 'red' };
   return (
     <>
       <h1 style={styleH1}>こんにちは！！</h1>
-      <p>お元気ですか。</p>
+      <p style={contentStyle}>お元気ですか。</p>
+      <p style={contentPinkStyle}>元気です！</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
