cde29df3ce71af5a0ca3e7ef3da4873fe7e5688b
Yuzunoha <studysteadystandby@gmail.com>
2022-06-15 20:45:23 +0900

    useState

--- chapter4/my-app/src/App.jsx
+++ chapter4/my-app/src/App.jsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import { ColoredMessage } from './components/ColoredMessage';
 
 export const App = () => {
+  // Stateの定義
+  const [num, setNum] = useState(0);
   // ボタン押下時に呼ぶ関数
   const onClickButton = () => {
-    alert();
+    setNum(num + 1);
   };
 
   const styleH1 = { color: 'red' };
@@ -13,6 +16,7 @@ export const App = () => {
       <ColoredMessage color='blue'>お元気ですか？</ColoredMessage>
       <ColoredMessage color='pink'>元気です！</ColoredMessage>
       <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
     </>
   );
 };
