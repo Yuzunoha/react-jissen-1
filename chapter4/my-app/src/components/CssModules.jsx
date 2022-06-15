9f70d7c893cf17d1b8616a678252d3c13ff0ab03
Yuzunoha <studysteadystandby@gmail.com>
2022-06-15 23:13:46 +0900

    両方出た

--- chapter4/my-app/src/components/CssModules.jsx
+++ chapter4/my-app/src/components/CssModules.jsx
@@ -1,10 +1,16 @@
 import classes from './CssModules.module.scss';
+import { StyledJsx } from './StyledJsx';
 
 export const CssModules = () => {
   return (
-    <div className={classes.container}>
-      <p className={classes.title}>CSS Modulesです</p>
-      <button className={classes.button}>ボタン</button>
-    </div>
+    <>
+      <div className={classes.container}>
+        <p className={classes.title}>CSS Modulesです</p>
+        <button className={classes.button}>ボタン</button>
+      </div>
+      <div>
+        <StyledJsx></StyledJsx>
+      </div>
+    </>
   );
 };
