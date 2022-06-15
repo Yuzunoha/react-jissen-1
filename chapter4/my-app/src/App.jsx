f03088163b863b1a25dcaa8a9829e1131f4441b7
Yuzunoha <studysteadystandby@gmail.com>
2022-06-15 23:27:12 +0900

    整理した

--- chapter4/my-app/src/App.jsx
+++ chapter4/my-app/src/App.jsx
@@ -1,5 +1,13 @@
 import { CssModules } from './components/CssModules';
+import { StyledJsx } from './components/StyledJsx';
+import { StyledComponents } from './components/StyledComponents';
 
 export const App = () => {
-  return <CssModules></CssModules>;
+  return (
+    <>
+      <CssModules></CssModules>
+      <StyledJsx></StyledJsx>
+      <StyledComponents></StyledComponents>
+    </>
+  );
 };
--- chapter4/my-app/src/components/CssModules.jsx
+++ chapter4/my-app/src/components/CssModules.jsx
@@ -1,16 +1,10 @@
 import classes from './CssModules.module.scss';
-import { StyledJsx } from './StyledJsx';
 
 export const CssModules = () => {
   return (
-    <>
-      <div className={classes.container}>
-        <p className={classes.title}>CSS Modulesです</p>
-        <button className={classes.button}>ボタン</button>
-      </div>
-      <div>
-        <StyledJsx></StyledJsx>
-      </div>
-    </>
+    <div className={classes.container}>
+      <p className={classes.title}>CSS Modulesです</p>
+      <button className={classes.button}>ボタン</button>
+    </div>
   );
 };
