485727aebd2633435ffded6340f91c56790bf1d5
Yuzunoha <studysteadystandby@gmail.com>
2022-06-16 01:12:41 +0900

    aaa

--- chapter4/my-app/tailwind.config.js
+++ chapter4/my-app/tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
   content: [],
   theme: {
     extend: {},
