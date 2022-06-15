bee58cdb05b5b429170eed5e451ada849863f142
Yuzunoha <studysteadystandby@gmail.com>
2022-06-16 01:15:17 +0900

    コピペ

--- chapter4/my-app/tailwind.config.js
+++ chapter4/my-app/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false,
-  content: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
