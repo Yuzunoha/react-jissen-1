ac87d217e8a60b8b35d7424faa32585860f16fdf
Yuzunoha <studysteadystandby@gmail.com>
2022-06-15 20:35:08 +0900

    pinkもコンポーネント利用にした

--- chapter4/my-app/src/App.jsx
+++ chapter4/my-app/src/App.jsx
@@ -6,18 +6,12 @@ export const App = () => {
     alert();
   };
 
-  // ピンクように追加
-  const contentPinkStyle = {
-    color: 'pink',
-    fontSize: '20px',
-  };
-
   const styleH1 = { color: 'red' };
   return (
     <>
       <h1 style={styleH1}>こんにちは！！</h1>
       <ColoredMessage color='blue' message='お元気ですか？' />
-      <p style={contentPinkStyle}>元気です！</p>
+      <ColoredMessage color='pink' message='元気です！' />
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
