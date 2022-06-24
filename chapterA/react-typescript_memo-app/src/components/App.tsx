0c55408c7943734b21622365da1cb72fdb4ab880
Yuzunoha <studysteadystandby@gmail.com>
2022-06-25 01:30:43 +0900

    途中

--- chapterA/react-typescript_memo-app/src/components/App.tsx
+++ chapterA/react-typescript_memo-app/src/components/App.tsx
@@ -1,3 +1,12 @@
-export const App = () => {
+import { ChangeEvent, useState, FC } from 'react';
+import styled from 'styled-components';
+
+export const App: FC = () => {
+  // テキストボックス State
+  const [text, setText] = useState<string>('');
+
+  // メモ一覧 State
+  const [memos, setMemos] = useState<string[]>([]);
+
   return <h1>簡易メモアプリ</h1>;
 };
