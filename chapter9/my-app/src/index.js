e86754824ffce0f7b3f5f49c882f9a2e3ed63c32
Yuzunoha <studysteadystandby@gmail.com>
2022-06-24 15:54:41 +0900

    動いた

--- chapter9/my-app/src/index.js
+++ chapter9/my-app/src/index.js
@@ -1,10 +1,4 @@
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { AdminFlagProvider } from './components/providers/AdminFlagProvider';
 
-ReactDOM.render(
-  <AdminFlagProvider>
-    <App />
-  </AdminFlagProvider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
