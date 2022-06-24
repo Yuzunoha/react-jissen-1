21cce2a94311f2773d7e92673dd44c9122cce7d6
Yuzunoha <studysteadystandby@gmail.com>
2022-06-24 16:06:17 +0900

    リファクタリング

--- chapter9/my-app/src/App.js
+++ chapter9/my-app/src/App.js
@@ -12,26 +12,33 @@ export const App = () => {
     setIsLoading(true);
     setIsError(false);
 
+    // リザルトをユーザリストに格納する関数
+    const setUserListFromResult = (result) => {
+      const users = result.data.map((user) => ({
+        id: user.id,
+        name: `${user.lastname} ${user.firstname}`,
+        age: user.age,
+      }));
+      setUserList(users);
+    };
+
     // APIを叩く
     const url = 'https://markdown.yuzunoha.net/api/react/chapter9';
     axios
       .get(url)
-      .then((result) => {
-        const users = result.data.map((user) => ({
-          id: user.id,
-          name: `${user.lastname} ${user.firstname}`,
-          age: user.age,
-        }));
-        setUserList(users);
-      })
+      .then(setUserListFromResult)
       .catch(() => setIsError(true))
       .finally(() => setIsLoading(false));
   };
+
+  // ユーザリストをhtmlにする関数
+  const dispUserList = () => userList.map((user) => <p key={user.id}>{`${user.id}:${user.name}(${user.age}歳)`}</p>);
+
   return (
     <div>
       <button onClick={onClickFetchUser}>ユーザー取得</button>
       {isError && <p style={{ color: 'red' }}>エラーが発生しました</p>}
-      {isLoading ? <p>データ取得中です</p> : userList.map((user) => <p key={user.id}>{`${user.id}:${user.name}(${user.age}歳)`}</p>)}
+      {isLoading ? <p>データ取得中です</p> : dispUserList()}
     </div>
   );
 };
