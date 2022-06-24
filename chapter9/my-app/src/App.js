154e83eedbfdf0563c022a3c2f4491afa8148d3d
Yuzunoha <studysteadystandby@gmail.com>
2022-06-24 16:48:55 +0900

    動いた

--- chapter9/my-app/src/App.js
+++ chapter9/my-app/src/App.js
@@ -1,12 +1,7 @@
-import { useState } from 'react';
 import { useFetchUsers } from './hooks/useFetchUsers';
 
 export const App = () => {
-  const { userList, onClickFetchUser } = useFetchUsers();
-  console.log(userList);
-
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
+  const { userList, isLoading, isError, onClickFetchUser } = useFetchUsers();
 
   // ユーザリストをhtmlにする関数
   const dispUserList = () => userList.map((user) => <p key={user.id}>{`${user.id}:${user.name}(${user.age}歳)`}</p>);
--- chapter9/my-app/src/hooks/useFetchUsers.js
+++ chapter9/my-app/src/hooks/useFetchUsers.js
@@ -1,9 +1,35 @@
 import { useState } from 'react';
+import axios from 'axios';
 
-// ユーザー一覧を取得するカスタムフック
+// ユーザー取得ボタン押下アクション
 export const useFetchUsers = () => {
-  const [userList, setUserList] = useState([{ id: 1 }]);
-  const onClickFetchUser = () => alert('関数実行');
+  const [userList, setUserList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-  return { userList, onClickFetchUser };
+  const onClickFetchUser = () => {
+    // ボタン押下時
+    setIsLoading(true);
+    setIsError(false);
+
+    // リザルトをユーザリストに格納する関数
+    const setUserListFromResult = (result) => {
+      const users = result.data.map((user) => ({
+        id: user.id,
+        name: `${user.lastname} ${user.firstname}`,
+        age: user.age,
+      }));
+      setUserList(users);
+    };
+
+    // APIを叩く
+    const url = 'https://markdown.yuzunoha.net/api/react/chapter9';
+    axios
+      .get(url)
+      .then(setUserListFromResult)
+      .catch(() => setIsError(true))
+      .finally(() => setIsLoading(false));
+  };
+
+  return { userList, isLoading, isError, onClickFetchUser };
 };
